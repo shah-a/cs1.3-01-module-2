47f7306149c6e284e43e44f624454153d2f4d642
Ali Shah <s.ashah@outlook.com>
2021-06-27 07:00:35 -0400

    [Finished] `prepend()`, `delete()`, and `find()`

--- LinkedList.py
+++ LinkedList.py
@@ -37,19 +37,60 @@ class LinkedList:
 
   def prepend(self, data):
     '''Add a new node to the beginning of the linked list'''
-    pass
+    new_node = Node(data)
+
+    # what if my linked list is empty?
+    if self.head is None:
+      self.head = new_node
+      self.tail = new_node
+      return
+
+    new_node.next = self.head
+    self.head = new_node
 
   def delete(self, item):
     '''Remove a specific data item from the linked list'''
-    pass
+    current = self.head
+    previous = None
+
+    while current is not None:
+      # what if the item to delete is the only item in my linked list? 
+      if current.data == item and previous is None and current.next is None:
+        self.head = None
+        self.tail = None
+        break
+
+      # what if the item to delete is my linked list's head?
+      if current.data == item and previous is None:
+        self.head = current.next
+        break
+
+      # what if the item to delete is my linked list's tail?
+      if current.data == item and current.next is None:
+        self.tail = previous
+        previous.next = current.next  # will be set to None, since tail's current.next == None
+        break
+
+      # what if the item to delete is somewhere in the middle of 3+ items?
+      if current.data == item and previous is not None:
+        previous.next = current.next
+        break
+
+      previous = current
+      current = current.next
 
   def find(self, item):
     '''Find a specific item in the linked list, return the node, if not found return None'''
-    pass
+    current = self.head
+
+    while current is not None:
+      if current.data == item:
+        return current
+      current = current.next
+
+    return None
 
   def print_list(self):
-    
-    #TODO: try to implement 
     #start with the head
     current = self.head
 
--- unit_tests.py
+++ unit_tests.py
@@ -33,3 +33,37 @@ def test_delete():
 
   my_ll.delete(30)
   assert my_ll.head.next.next.data == 55
+
+def test_delete_v2():
+  my_ll = LinkedList()
+  my_ll.append(10)
+  my_ll.delete(10)
+  assert my_ll.head == None
+  assert my_ll.tail == None
+
+  my_ll = LinkedList()
+  my_ll.append(10)
+  my_ll.append(20)
+  my_ll.delete(20)
+  assert my_ll.head.data == 10
+  assert my_ll.tail.data == 10
+
+  my_ll = LinkedList()
+  my_ll.append(10)
+  my_ll.append(20)
+  my_ll.delete(10)
+  assert my_ll.head.data == 20
+  assert my_ll.tail.data == 20
+
+if __name__ == '__main__':
+  test_prepend()
+  print("`prepend()` tests passed")
+
+  test_find()
+  print("`find()` tests passed")
+
+  test_delete()
+  print("`delete()` tests passed")
+
+  test_delete_v2()
+  print("`delete()` tests (v2) passed")
